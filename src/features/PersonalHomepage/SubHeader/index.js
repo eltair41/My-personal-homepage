6a453b16a7900c63e80d48641622ed98f7e94678
Karol Galera <frontend.galera@gmail.com>
2022-09-20 17:37:51 +0200

    component SubHeader added

---
+++ src/features/PersonalHomepage/SubHeader/index.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const SubHeader = styled.h2`
+  font-size: 30px;
+  font-weight: 900;
+  margin: 0;
+  color: ${({theme}) => theme.colors.textPrimary};
+
+  @media(max-width: ${({theme}) => theme.breakpoints.mobileMax}px) {
+    font-size: 18px;
+  }
+`;
