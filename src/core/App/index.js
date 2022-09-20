1b7f2a169d4edd1242724a90dfb862d6ca56d889
Karol Galera <frontend.galera@gmail.com>
2022-09-20 13:38:41 +0200

    component PersonalHomepage added

--- src/core/App/index.js
+++ src/core/App/index.js
@@ -1,15 +1,15 @@
 import { ThemeProvider } from "styled-components";
-import { themeDark, themeLight } from "./theme";
+import { themeLight } from "./theme";
 import { Normalize } from "styled-normalize";
 import { GlobalStyle } from "./GlobalStyle";
-import ThemeSwitch from "../../common/ThemeSwitch";
+import PersonalHomepage from "../../features/PersonalHomepage";
 
 const App = () => {
   return (
     <ThemeProvider theme={themeLight}>
       <Normalize />
       <GlobalStyle />
-      <ThemeSwitch />
+      <PersonalHomepage />
     </ThemeProvider>
   );
 }
---
+++ src/features/PersonalHomepage/index.js
@@ -0,0 +1,12 @@
+import ThemeSwitch from "../../common/ThemeSwitch";
+import { Container } from "./styled";
+
+const PersonalHomepage = () => {
+  return (
+    <Container>
+      <ThemeSwitch />
+    </Container>
+  )
+};
+
+export default PersonalHomepage;
