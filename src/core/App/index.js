373b63ec70d81588d8a8ae7a554ce119bbdfcd7c
Karol Galera <frontend.galera@gmail.com>
2022-09-20 12:56:26 +0200

    Normalize added to app

--- src/core/App/index.js
+++ src/core/App/index.js
@@ -1,9 +1,11 @@
 import { ThemeProvider } from "styled-components";
 import { themeDark, themeLight } from "./theme";
+import { Normalize } from "styled-normalize";
 
 const App = () => {
   return (
     <ThemeProvider theme={themeLight}>
+      <Normalize />
       Hello World
     </ThemeProvider>
   );
