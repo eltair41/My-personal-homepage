a2a4bb6a8d531a12ea8c71d107e1c08caf98e9cd
Karol Galera <frontend.galera@gmail.com>
2022-09-21 18:43:55 +0200

    selectors and dsipatch added to Portfolio

--- src/features/PersonalHomepage/Portfolio/index.js
+++ src/features/PersonalHomepage/Portfolio/index.js
@@ -1,7 +1,20 @@
+import { downloadRepositoriesPending, selectRepositories, selectRepositoriesStatus } from "../../personalHomepageSlice";
+import { useSelector, useDispatch } from "react-redux";
 import { MyRecentProjects, Section, StyledIcon, Header } from "./styled";
 import { SubHeader } from "../SubHeader";
+import Content from "./Content";
+import { useEffect } from "react";
+import { githubUsername } from "./githubUsername";
 
 const Portfolio = () => {
+  const repositoriesStatus = useSelector(selectRepositoriesStatus);
+  const repositories = useSelector(selectRepositories);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(downloadRepositoriesPending(githubUsername));
+  }, [dispatch]);
+
   return (
     <Section>
       <Header>
@@ -9,6 +22,10 @@ const Portfolio = () => {
         <SubHeader>Portfolio</SubHeader>
         <MyRecentProjects>My recent projects</MyRecentProjects>
       </Header>
+      <Content
+        status={repositoriesStatus}
+        repositories={repositories}
+      />
     </Section>
   )
 };
