6c44a89386cf7deb381f3f4889d8c42f99719b98
Karol Galera <frontend.galera@gmail.com>
2022-09-21 15:35:21 +0200

    middleware added to store

--- src/core/store.js
+++ src/core/store.js
@@ -1,12 +1,19 @@
 import { configureStore } from '@reduxjs/toolkit';
 import themeReducer from '../common/themeSlice';
 import personalHomepageReducer from '../features/personalHomepageSlice';
+import createSagaMiddleware from '@redux-saga/core';
+import rootSaga from './rootSaga';
+
+const sagaMiddleware = createSagaMiddleware();
 
 const store = configureStore({
   reducer: {
     theme: themeReducer,
     personalHomepage: personalHomepageReducer,
   },
+  middleware: [sagaMiddleware],
 });
+
+sagaMiddleware.run(rootSaga);
 
 export default store;
