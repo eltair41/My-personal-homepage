8bbe849945706706642eeb543c63815b82f80884
Karol Galera <frontend.galera@gmail.com>
2022-09-22 14:16:52 +0200

    correction in paragraph

--- src/features/PersonalHomepage/Footer/index.js
+++ src/features/PersonalHomepage/Footer/index.js
@@ -15,7 +15,7 @@ const Footer = () => {
         <Paragraph>
           I"m looking forward to improving and learning new skills at FrontEnd.
           If you want to give me a chance to demonstrate my knowledge and hire me, please contact me by e-mail or other way.
-          I am gladly waiting for any challenges and job opportunities.
+          I am gladly waiting for any challenges and jobs opportunities.
         </Paragraph>
       </Address>
       <SocialIcons />
