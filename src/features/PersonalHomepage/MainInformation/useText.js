aa6e702cec16c29367854899e87d357fbf8c688f
Karol Galera <frontend.galera@gmail.com>
2023-05-11 19:43:02 +0200

    useText custom Hook that animates text display, added

---
+++ src/features/PersonalHomepage/MainInformation/useText.js
@@ -0,0 +1,48 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
+const useText = (phrases) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [isDeleting, setIsDeleting] = useState(false);
+  const [text, setText] = useState("");
+  const [delay, setDelay] = useState(80);
+
+  useEffect(() => {
+    const loop = () => {
+      let i = currentIndex % phrases.length;
+      let fullText = phrases[i];
+      const updateText = !isDeleting
+        ? fullText.substring(0, text.length + 1)
+        : fullText.substring(0, text.length - 1);
+
+      setText(updateText);
+
+      if (!isDeleting && text === fullText) {
+        const wait = setTimeout(() => {
+          setIsDeleting(true);
+          setDelay(40);
+        }, 3000);
+
+        return () => clearTimeout(wait);
+      } else if (isDeleting && text === "") {
+        const start = setTimeout(() => {
+          setIsDeleting(false);
+          setDelay(80);
+          setCurrentIndex(currentIndex + 1);
+        }, 2000);
+
+        return () => clearTimeout(start);
+      }
+    };
+
+    let ticker = setInterval(() => {
+      loop();
+    }, delay);
+
+    return () => clearInterval(ticker);
+  }, [text, phrases, isDeleting, delay]);
+
+  return text;
+};
+
+export default useText;
