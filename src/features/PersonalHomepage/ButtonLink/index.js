e27f0363e37a81f32f9b9103c568ebcdaf654b55
Karol Galera <frontend.galera@gmail.com>
2023-05-12 17:48:21 +0200

    animation on hover added to ButtonLink styled component

--- src/features/PersonalHomepage/ButtonLink/index.js
+++ src/features/PersonalHomepage/ButtonLink/index.js
@@ -1,21 +1,38 @@
 import styled from "styled-components";
 
 export const ButtonLink = styled.a`
+  position: relative;
   text-decoration: none;
   font-weight: 600;
   font-size: 20px;
-  padding: 12px 16px;
+  padding: 16px 18px;
   color: ${({ theme }) => theme.colors.buttonLink.text};
-  border: 1px solid ${({ theme }) => theme.colors.buttonLink.border};
   background: ${({ theme }) => theme.colors.primary};
+  border: 2px solid ${({ theme }) => theme.colors.buttonLink.border};
   border-radius: ${({ theme }) => theme.borderRadiusSmall};
-  transition: box-shadow 0.3s;
+  z-index: 1;
 
   &:hover {
-    box-shadow: 0 0 0 2px ${({ theme }) => theme.colors.buttonLink.shadow};
+    color: ${({ theme }) => theme.colors.primary};
   }
 
-  @media(max-width: ${({theme}) => theme.breakpoints.mobileMax}px) {
+  &::before {
+    content: "";
+    position: absolute;
+    background: ${({ theme }) => theme.colors.buttonLink.hover};
+    top: 0;
+    left: 0;
+    width: 0;
+    height: 100%;
+    z-index: -1;
+    transition: 0.5s;
+  }
+
+  &:hover::before {
+    width: 100%;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
     font-size: 18px;
   }
 `;
