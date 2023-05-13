a2adbd97a8befc02d7cee5a169e0ec225bb21957
Karol Galera <frontend.galera@gmail.com>
2023-05-13 19:25:33 +0200

    skillsData.js refactoring

--- src/features/PersonalHomepage/skillsData.js
+++ src/features/PersonalHomepage/skillsData.js
@@ -1,41 +1,50 @@
-export const skills = [
-  "Semantic & accesible HTML",
+export const restSkills = [
   "Markdown",
   "Git",
-  "Responsive web Design",
-  "React",
-  "Java Script: ES6+",
   "Type Script (basics)",
   "GitHub Pull Request & Review",
-  "React Hooks",
   "NPM",
-  "Styled-Components",
-  "React Redux",
-  "Redux (Toolkit)",
-  "Immutability",
-  "Working with API (fetch, axios)",
-  "Promises, Async/Await",
-  "Error handling",
-  "Redux-Saga",
-  "React Router",
-  "CSS Grid",
-  "CSS Flexbox",
-  "CSS BEM convetion",
-  "Teamwork",
   "Trello",
   "Figma",
   "Scrum",
-  "Next.js",
   "GraphQL",
   "Apollo Client",
   "MongoDB",
+];
+
+export const reactSkills = [
+  "React Hooks",
+  "React Redux",
+  "Redux (Toolkit)",
+  "Redux-Saga",
+  "React Router",
   "React Context",
   "React Portals",
+  "Styled-Components",
+];
+
+export const nextSkills = [
   "Next hooks",
   "Static Site Generation",
   "Server Side Rendering",
   "API Routes",
   "Next-Auth",
+];
+
+export const jsSkills = [
+  "Immutability",
+  "Working with API (fetch, axios)",
+  "Promises, Async/Await",
+  "Error handling",
+  "Arrays/Objects",
+];
+
+export const htmlCssSkills = [
+  "Semantic & accesible HTML",
+  "Responsive web Design",
+  "CSS Grid",
+  "CSS Flexbox",
+  "CSS BEM convetion",
   "CSS modules",
   "CSS variables",
 ];
