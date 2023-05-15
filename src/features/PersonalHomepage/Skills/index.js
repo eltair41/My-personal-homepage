ff30bb665d5d30ee4cce9fa91385d6724b10f818
Karol Galera <frontend.galera@gmail.com>
2023-05-16 00:58:38 +0200

    SKills component with styles refactoring

--- src/features/PersonalHomepage/Skills/index.js
+++ src/features/PersonalHomepage/Skills/index.js
@@ -1,21 +1,48 @@
-import { Bullet, List, ListItem, Section, StyledHeaader } from "./styled";
+import {
+  htmlCssSkills,
+  jsSkills,
+  nextSkills,
+  reactSkills,
+  restSkills,
+} from "./skillsData";
+import TechSkills from "./TechSkills";
+import { Section, StyledHeaader } from "./styled";
+import { skillsName } from "./skillsName";
 
-const Skills = ({ title, skills }) => {
+const Skills = ({ title }) => {
   return (
-    <Section>
+    <>
       <StyledHeaader>{title}</StyledHeaader>
-      <List>
-        {skills.map(skill => (
-          <ListItem
-            key={skill}
-          >
-            <Bullet />
-            {skill}
-          </ListItem>
-        ))}
-      </List>
-    </Section>
-  )
+      <Section>
+        <TechSkills
+          name={skillsName.react.name}
+          icon={<skillsName.react.Icon />}
+          skillArray={reactSkills}
+        />
+        <TechSkills
+          name={skillsName.next.name}
+          icon={<skillsName.next.Icon />}
+          skillArray={nextSkills}
+        />
+        <TechSkills
+          name={skillsName.js.name}
+          icon={<skillsName.js.Icon />}
+          skillArray={jsSkills}
+        />
+        <TechSkills
+          name={skillsName.htmlCss.name}
+          icon={<skillsName.htmlCss.Icon />}
+          secondIcon={<skillsName.htmlCss.SecondIcon />}
+          skillArray={htmlCssSkills}
+        />
+        <TechSkills
+          name={skillsName.other.name}
+          icon={<skillsName.other.Icon />}
+          skillArray={restSkills}
+        />
+      </Section>
+    </>
+  );
 };
 
 export default Skills;
