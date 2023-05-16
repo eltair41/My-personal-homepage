a6afaee65a1b64df155455baf9609e0264c3586c
Karol Galera <frontend.galera@gmail.com>
2023-05-16 17:13:09 +0200

    iconColor added to common object in theme

--- src/core/App/theme.js
+++ src/core/App/theme.js
@@ -23,6 +23,7 @@ const common = {
   boxShadow:
     "0px -2px 50px rgba(9, 10, 51, 0.02), 0px 16px 58px rgba(9, 10, 51, 0.03)",
   borderRadiusSmall: "4px",
+  iconColor: colorNames.white,
 };
 
 export const themeLight = {
