9b5d32f0f976645b5493657d2702f56a15de81ff
Karol Galera <frontend.galera@gmail.com>
2023-05-16 17:19:13 +0200

    code optimalized in Skills component

--- src/features/PersonalHomepage/Skills/index.js
+++ src/features/PersonalHomepage/Skills/index.js
@@ -1,45 +1,15 @@
-import {
-  htmlCssSkills,
-  jsSkills,
-  nextSkills,
-  reactSkills,
-  restSkills,
-} from "./skillsData";
+import { skillData } from "../skillsData";
 import TechSkills from "./TechSkills";
 import { Section, StyledHeaader } from "./styled";
-import { skillsName } from "./skillsName";
 
 const Skills = ({ title }) => {
   return (
     <>
       <StyledHeaader>{title}</StyledHeaader>
       <Section>
-        <TechSkills
-          name={skillsName.react.name}
-          icon={<skillsName.react.Icon />}
-          skillArray={reactSkills}
-        />
-        <TechSkills
-          name={skillsName.next.name}
-          icon={<skillsName.next.Icon />}
-          skillArray={nextSkills}
-        />
-        <TechSkills
-          name={skillsName.js.name}
-          icon={<skillsName.js.Icon />}
-          skillArray={jsSkills}
-        />
-        <TechSkills
-          name={skillsName.htmlCss.name}
-          icon={<skillsName.htmlCss.Icon />}
-          secondIcon={<skillsName.htmlCss.SecondIcon />}
-          skillArray={htmlCssSkills}
-        />
-        <TechSkills
-          name={skillsName.other.name}
-          icon={<skillsName.other.Icon />}
-          skillArray={restSkills}
-        />
+        {skillData.map((data) => (
+          <TechSkills key={data.name} {...data} />
+        ))}
       </Section>
     </>
   );
